aa6afbae665a56947436f6c61b0290c2805a1b0b
Marcos Sánchez Ocegueda <marcos.sanchez.dev@gmail.com>
2024-10-22 23:50:31 -0600

    Update myAtoi.js

--- String to Integer (atoi)/myAtoi.js
+++ String to Integer (atoi)/myAtoi.js
@@ -1,35 +1,33 @@
 var myAtoi = function (s) {
-  let deleteSignedness = ""
-  let newS = ""
-  let finalS = ""
-
   s = s.trim()
+  if (s.length == 0) return 0
 
-  if (s[0] === "-") {
-    deleteSignedness = s.substring(1)
-  } else {
-    deleteSignedness = s
+  let i = 0
+  let sign = 1
+
+  if (s[0] == "-") {
+    sign = -1
+    i = 1
+  } else if (s[0] == "+") {
+    i = 1
   }
 
-  for (let char of deleteSignedness) {
-    if (Number(char) !== 0) {
-      newS += char
-    }
+  let num = 0
+
+  while (i < s.length && s[i] >= "0" && s[i] <= "9") {
+    num = num * 10 + (s[i] - "0")
+    i++
   }
 
-  for (let char of newS) {
-    if (Number(char)) {
-      finalS += char
-    } else {
-      return finalS
-    }
-  }
-  
-  if(Number(finalS)) return finalS
+  const intMax = Math.pow(2, 31) - 1
+  const intMin = -Math.pow(2, 31)
+  num *= sign
+  if (num > intMax) return intMax
+  if (num < intMin) return intMin
 
-  return 0
+  return num
 }
 
-const result = myAtoi(" ")
+const result = myAtoi("-91283472332")
 
 console.log(result)
